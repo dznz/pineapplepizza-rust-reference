69cb2d87afe910f7b5a13567393e6089e3b2727b
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-11-20 19:39:23 +1300

    Add start of data structures.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[macro_use]
+extern crate nom;
+
+use std::option;
+
+#[derive(Debug,PartialEq)]
+pub struct StructuredListItem<'a> {
+  name: &'a str
+}
+
+#[derive(Debug,PartialEq)]
+pub struct StructuredCollection<'a> {
+  level: u8,
+  name: &'a str,
+  text: Option<&'a str>,
+  ol: Vec<StructuredListItem<'a>>,
+  ul: Vec<StructuredListItem<'a>>,
+  headings: Vec<StructuredCollection<'a>>
+}
+
 fn main() {
     println!("Hello, world!");
 }
