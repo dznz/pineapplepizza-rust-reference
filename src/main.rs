b8203aaa535ddf20bee2f35dd604e8fbcf6dcbed
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-11-20 23:50:09 +1300

    We can now parse StructuredListItems.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@ use std::option;
 use std::collections::HashMap;
 use nom::IResult;
 
-#[derive(Debug,PartialEq)]
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
+#[derive(Debug,PartialEq,Eq,Clone)]
 pub struct StructuredListItem<'a> {
   pub name: &'a str,
   pub kv: HashMap<&'a str, &'a str>
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Eq,Clone)]
 pub struct StructuredCollection<'a> {
   level: u8,
   name: &'a str,
@@ -21,23 +27,70 @@ pub struct StructuredCollection<'a> {
   headings: Vec<StructuredCollection<'a>>
 }
 
-fn ul_wrapper<'a>(input: &'a str, sep: &'static str) -> IResult<&'a str, Vec<StructuredListItem<'a>>> {
-    many0!(input, do_parse!(
+fn ul_wrapper<'a>(input: &'a str, sepb: bool) -> IResult<&'a str, Vec<StructuredListItem<'a>>> {
+    let sep = (if sepb {"- "} else {"* "});
+    many1!(input, do_parse!(
             tag!(sep)  >>
-      name: take_till!(|ch| ch == '\r') >>
-            tag!("\r\n") >>
+      name: take_till!(|ch| ch == '\n') >>
+            tag!("\n") >>
       kv:   map!(many0!(do_parse!(tag!("  ") >>
-              val: separated_pair!(take_till!(|ch| ch == ':' || ch == '\r'), tag!(":"), take_till!(|ch| ch == ':' || ch == '\r')) >> (val))), |vec: Vec<_>| vec.into_iter().collect()) >>
+              val: separated_pair!(take_till!(|ch| ch == ':' || ch == '\n'), tag!(": "), take_till!(|ch| ch == ':' || ch == '\n')) >> tag!("\n") >> (val))), |vec: Vec<_>| vec.into_iter().collect()) >>
             (StructuredListItem { name: name, kv: kv })
     ))
 }
 
 named!(ul<&str, Vec<StructuredListItem>>,
   alt!(
-    apply!(ul_wrapper, "- ") | apply!(ul_wrapper, "* ")
+    apply!(ul_wrapper, false) | apply!(ul_wrapper, true)
   )
 );
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn parse_list_items() {
+  let sample_list_items = vec!(
+    StructuredListItem {
+      name: "Foo bar baz",
+      kv: [("key", "value")].iter().cloned().collect(),
+    },
+    StructuredListItem {
+      name: "Other stuff",
+      kv: [("Description", "Thing"), ("Caveat", "Stuff")].iter().cloned().collect(),
+    },
+    StructuredListItem {
+      name: "No kvs",
+      kv: [].iter().cloned().collect(),
+    },
+  );
+  let path = Path::new("list_items.🍍🍕.slice");
+  let display = path.display();
+
+  // Open the path in read-only mode, returns `io::Result<File>`
+  let mut file = match File::open(&path) {
+      // The `description` method of `io::Error` returns a string that
+      // describes the error
+      Err(why) => panic!("couldn't open {}: {}", display,
+                                                 why.description()),
+      Ok(file) => file,
+  };
+
+  // Read the file contents into a string, returns `io::Result<usize>`
+  let mut s = String::new();
+  match file.read_to_string(&mut s) {
+      Err(why) => panic!("couldn't read {}: {}", display,
+                                                 why.description()),
+      Ok(_) => print!("{} contains:\n{}", display, s),
+  }
+  let foo = s.clone();
+  let list_items_maybe = ul(&foo);
+  let list_items = match list_items_maybe {
+    Err(why) => panic!("{}", why.description()),
+    Ok((_, x))    => x.clone()
+  };
+  assert_eq!(list_items, sample_list_items)
+  //print!("{:?}", list_items_maybe); //assert_eq!(list_items, sample_list_items)
+    
 }
