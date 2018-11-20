4d04c7706610e8b66ad2474809e82b061099e10f
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-11-20 20:22:31 +1300

    Add Parsing for StructuredListItem s

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::option;
 
 #[derive(Debug,PartialEq)]
 pub struct StructuredListItem<'a> {
-  name: &'a str
+  pub name: &'a str
 }
 
 #[derive(Debug,PartialEq)]
@@ -17,6 +17,23 @@ pub struct StructuredCollection<'a> {
   ul: Vec<StructuredListItem<'a>>,
   headings: Vec<StructuredCollection<'a>>
 }
+
+named!(ul<&str, Vec<StructuredListItem>>,
+  alt!(
+    many0!(do_parse!(
+            tag!("- ")  >>
+      name: take_till!(|ch| ch == '\r') >>
+            tag!("\r\n") >>
+            (StructuredListItem { name: name })
+    )) |
+    many0!(do_parse!(
+            tag!("* ")  >>
+      name: take_till!(|ch| ch == '\r') >>
+            tag!("\r\n") >>
+            (StructuredListItem { name: name })
+    ))
+  )
+);
 
 fn main() {
     println!("Hello, world!");
